2bdfd7bb6ea4dc9a9f341ccf36854bd377a07780
sajalr24 <24698.sr@gmail.com>
2023-09-26 17:43:04 +0530

    assignment 1

---
+++ Assignment 1/Q1.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+
+struct Date
+{
+    int day;
+    int month;
+    int year;
+};
+
+void initDate(struct Date *ptrDate)
+{
+    ptrDate->day = 0;
+    ptrDate->month = 0;
+    ptrDate->year = 0;
+}
+
+void printDateOnConsole(struct Date *ptrDate)
+{
+    printf("Date: %02d/%02d/%04d\n", ptrDate->day, ptrDate->month, ptrDate->year);
+}
+
+void acceptDateFromConsole(struct Date *ptrDate)
+{
+    printf("Enter date (dd/mm/yyyy): ");
+    scanf("%d/%d/%d", &ptrDate->day, &ptrDate->month, &ptrDate->year);
+}
+
+int main()
+{
+    struct Date myDate;
+    int choice;
+
+    initDate(&myDate);
+
+    do
+    {
+        printf("\nMenu:\n");
+        printf("1. Initialize Date\n");
+        demo1
+            printf("2. Print Date\n");
+        printf("3. Accept Date\n");
+        printf("4. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice)
+        {
+        case 1:
+            initDate(&myDate);
+            printf("Date initialized.\n");
+            break;
+        case 2:
+            printDateOnConsole(&myDate);
+            break;
+        case 3:
+            acceptDateFromConsole(&myDate);
+            break;
+        case 4:
+            printf("Exiting program.\n");
+            break;
+        default:
+            printf("Invalid choice. Please try again.\n");
+        }
+    } while (choice != 4);
+
+    return 0;
+}
